477ceb12a2d03d7a0a7f8fd2dd461787d4c2f5ec
Yefry Ramirez <yaramirez68@gmail.com>
2026-02-13 15:09:45 -0500

    Initial commit cinemapedia app

---
+++ README.md
@@ -0,0 +1,5 @@
+# cinemapedia_app
+
+# Dev
+1. Copy the .env.template and renamed to .env
+2. Changes the enviroment variables (TheMovieDB)
---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  ThemeData applyTheme() =>
+      ThemeData(useMaterial3: true, colorSchemeSeed: Color.fromARGB(15, 251, 143, 254));
+}
